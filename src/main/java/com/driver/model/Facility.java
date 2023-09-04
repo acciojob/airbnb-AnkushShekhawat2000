c8773d906c183e84d1e62954028165e546124f17
Ashekhawat@2001 <ankush75056@gmail.com>
2023-09-04 16:49:57 +0530

    again

--- src/main/java/com/driver/model/Facility.java
+++ src/main/java/com/driver/model/Facility.java
@@ -3,26 +3,15 @@ package com.driver.model;
 import java.util.List;
 
 public enum Facility {
-    ;
 
-//    GYM,
-//    SWIMMING_POOL,
-//    BAR,
-//    FOOD,
-//    PARKING,
-//    LAUNDRY;
 
-   private List<Facility> facilities;
+    GYM,
+    SWIMMING_POOL,
+    BAR,
+    FOOD,
+    PARKING,
+    LAUNDRY,
 
-    Facility(List<Facility> facilities) {
-        this.facilities = facilities;
-    }
-
-    public List<Facility> getFacilities() {
-        return facilities;
-    }
-
-    public void setFacilities(List<Facility> facilities) {
-        this.facilities = facilities;
-    }
 }
+
+
